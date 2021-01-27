bb49c3ce7e87b2a9fe626f7420a011252a828ba0
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-01-27 15:29:47 +0200

    js file

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('hello this is my first JS coding practice');
+console.log('hello ' + 'every' + 'one')
+console.log(7 !== 8);
+var x=10;
+console.log(x +=10); // the result will be 20
+
+var statement ='coding is very good for you';
+alert('stay in touch with us')
+
+
+// next component will ask you about your email and age.
+var email = prompt('what is your email');
+alert('accepted' + email);
+
+
+var age = prompt('how old are you?')
+console.log(age);
+
+if (age > 18 ) { message = 'welcome to our coding website';}
+
+   else if (age <18 ) { message = 'go to school';}
+   
+   else { message = 'always welcome';}
+
+document.write('<h1>' + message + '<h1>');
+
+
+
+
